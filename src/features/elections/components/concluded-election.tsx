048d44af514e3da6ee90abda927a250c7120fe5c
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 15:01:31 +0100

    added components as mockdata

--- src/features/elections/components/concluded-election.tsx
+++ src/features/elections/components/concluded-election.tsx
@@ -1,12 +1,17 @@
+import ElectionOptions from "./election-options";
+
 export default function ConcludedElection() {
   return (
-    <div className="collapse collapse-arrow join-item border-base-300 border">
+    <div className="collapse collapse-arrow join-item border-base-300 border ">
       <input type="radio" name="my-accordion-4" defaultChecked />
       <div className="collapse-title text-xl font-medium flex justify-between">
-        <h3 className="p-1">Election subject</h3>
+        <h3 className="p-1 text-black font-bold">Election subject</h3>
       </div>
       <div className="collapse-content flex flex-row justify-between">
-        <article>hej</article>
+        <article>
+          <ElectionOptions />
+          <ElectionOptions />
+        </article>
       </div>
     </div>
   );
