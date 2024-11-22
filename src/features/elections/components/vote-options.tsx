ef7ffeaf10e701316b38f6b37e75287d473ff042
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 13:37:35 +0100

    made component for the vote options in the election

---
+++ src/features/elections/components/vote-options.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function VoteOptions() {
+  return (
+    <div className="flex flex-row m-2 ">
+      <h3 className="p-1">Vote Option</h3>
+      <button className="btn btn-sm rounded-md">Vote</button>
+    </div>
+  );
+}
