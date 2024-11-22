b721c004e20002b76141e61c462c033b7c4ae2ad
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 13:48:44 +0100

    layout component for concluded elections

---
+++ src/features/elections/components/concluded-election-board.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ConcludedElectionBoard({ children }: Props) {
+  return (
+    <section className="container mx-auto w-10/12 bg-slate-100 flex flex-col my-4 rounded-md p-4">
+      <header className="grid grid-cols-4 gap-4 border-b-2">
+        <p>Topic</p>
+        <p className="text-center">Conclude Vote</p>
+      </header>
+      <div className="join join-vertical w-full">{children}</div>
+    </section>
+  );
+}
