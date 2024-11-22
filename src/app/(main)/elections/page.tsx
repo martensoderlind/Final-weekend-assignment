a0f1d4392b4b5313bdbab99801e1af45519867dc
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 13:18:35 +0100

    display new components

--- src/app/(main)/elections/page.tsx
+++ src/app/(main)/elections/page.tsx
@@ -1,3 +1,15 @@
-export default function page() {
-  return <div>Election</div>;
+import Election from "@/features/elections/components/election";
+import ElectionBoard from "@/features/elections/components/election-board";
+import ElectionForm from "@/features/elections/components/election-form";
+
+export default function Page() {
+  return (
+    <>
+      <ElectionForm />
+      <ElectionBoard>
+        <Election />
+        <Election />
+      </ElectionBoard>
+    </>
+  );
 }
