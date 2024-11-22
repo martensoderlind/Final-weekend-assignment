40fe0b119fd3b0a8e0c7ce6c7457b84a0c2dc3d2
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 15:00:40 +0100

    representativ election result component

---
+++ src/features/elections/components/election-options.tsx
@@ -0,0 +1,16 @@
+import RepresentativElectionResult from "./representativ-election-result";
+
+export default function ElectionOptions() {
+  return (
+    <div className="mt-2">
+      <h3 className="text-l text-gray-800 font-semibold">Election option</h3>
+      <header className="grid grid-cols-3 gap-4 border-b-2">
+        <h3>Representativ</h3>
+        <h3 className="text-center">Votes</h3>
+        <h3 className="text-center">Agreement</h3>
+      </header>
+      <RepresentativElectionResult />
+      <RepresentativElectionResult />
+    </div>
+  );
+}
