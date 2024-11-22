820a765557d4b77fdea5a8f73b0d9ad351ae94b3
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 13:02:14 +0100

    aligned headers with content

--- src/features/elections/components/representativ-board.tsx
+++ src/features/elections/components/representativ-board.tsx
@@ -9,9 +9,9 @@ export default function RepresentativBoard({ children }: Props) {
     <section className="container mx-auto w-10/12 bg-slate-100 flex flex-col my-4 rounded-md p-4">
       <header className="grid grid-cols-4 gap-4 border-b-2">
         <p>Name</p>
-        <p>Agreement</p>
-        <p>Votes</p>
-        <p>Pick representativ</p>
+        <p className="text-center">Agreement</p>
+        <p className="text-center">Votes</p>
+        <p className="text-center">Pick representativ</p>
       </header>
 
       {children}
