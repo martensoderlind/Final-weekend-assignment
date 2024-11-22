4fb0f4e1ef37210828b3ee4e811d1fc677121a96
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 12:51:37 +0100

    monir cleanup

--- src/app/(main)/concluded-elections/page.tsx
+++ src/app/(main)/concluded-elections/page.tsx
@@ -1,3 +1,3 @@
 export default function page() {
-  return <div>Concluded elections</div>;
+  return <div>Concluded Elections</div>;
 }
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -3,24 +3,22 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
+    <nav className="navbar bg-slate-950">
       <div className="">
         <h1 className="text-2xl">Title</h1>
       </div>
-      <div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <Link href="\representativs\">Representativs</Link>
-            </li>
-            <li>
-              <Link href="\elections\">Elections</Link>
-            </li>
-            <li>
-              <Link href="\concluded-elections\">Concluded elections</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-2 ">
+          <li className="px-2">
+            <Link href="\representativs\">Representativs</Link>
+          </li>
+          <li>
+            <Link href="\elections\">Elections</Link>
+          </li>
+          <li>
+            <Link href="\concluded-elections\">Concluded elections</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
