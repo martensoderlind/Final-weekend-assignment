d4b598a96a7bac47d60c34ebd1c8b2cbf2f3c360
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 12:50:14 +0100

    component for representativ

---
+++ src/features/elections/components/representativ.tsx
@@ -0,0 +1,10 @@
+export default function Representativ() {
+  return (
+    <article className="grid grid-cols-4 gap-4 my-2">
+      <p className="">Mårten</p>
+      <p className="text-center">90%</p>
+      <p className="text-center">50</p>
+      <input type="checkbox" className="" />
+    </article>
+  );
+}
