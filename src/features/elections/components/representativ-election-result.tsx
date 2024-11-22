4e0d746c38efcd355d0b5f81269bf3a7e453127b
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 15:00:09 +0100

    representativ election result component

---
+++ src/features/elections/components/representativ-election-result.tsx
@@ -0,0 +1,9 @@
+export default function RepresentativElectionResult() {
+  return (
+    <div className="grid grid-cols-3 gap-4 ">
+      <p>Rep name</p>
+      <p className="text-center">number of votes</p>
+      <p className="text-center">67%</p>
+    </div>
+  );
+}
