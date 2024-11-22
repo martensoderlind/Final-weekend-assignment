289be9720acb611183be60921d68377d50a69172
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 19:57:00 +0100

    new repository function

--- src/features/elections/repository.ts
+++ src/features/elections/repository.ts
@@ -1,9 +1,12 @@
-import { Representatives, representatives } from "./fixtures/mockdb";
+import { Representatives, representatives, voters } from "./fixtures/mockdb";
 
 export function createRepository() {
   return {
     async getAllRepresentatives() {
       return representatives;
+    },
+    async getAllVoters() {
+      return voters;
     },
     async createRepresentative(representative: Representatives) {
       representatives.push(representative);
