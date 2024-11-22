f1f7d4509a222ac43929538976148159829ba904
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 11:54:53 +0100

    added routes for the three pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <p>final project</p>
+      <Link href="\representativs\">Welcome to the app</Link>
     </div>
   );
 }
