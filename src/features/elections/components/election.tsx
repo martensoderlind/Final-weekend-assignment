d0a83827c3da91bee854383e7a9c6e786e3e6a8f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 21:29:10 +0100

    renamed component and changed mock election name to  the the name from the mock db

--- src/features/elections/components/election.tsx
+++ src/features/elections/components/election.tsx
@@ -1,15 +1,15 @@
+import { Election } from "../fixtures/mockdb";
 import VoteOptions from "./vote-options";
 
-export default function Election() {
+export default function ActiveElection({ election }: { election: Election }) {
   return (
     <div className="collapse collapse-arrow join-item border-base-300 border">
       <input type="radio" name="my-accordion-4" defaultChecked />
       <div className="collapse-title text-xl font-medium flex justify-between">
-        <h3 className="p-1">Election subject</h3>
+        <h3 className="p-1">{election.subject}</h3>
       </div>
       <div className="collapse-content flex flex-row justify-between">
         <article>
-          <VoteOptions />
           <VoteOptions />
         </article>
         <button className="btn aling self-end rounded-md">Conclude Vote</button>
