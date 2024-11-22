6e82620a416a32d4e8ef7865a0004e951119d065
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 15:01:02 +0100

    styling

--- src/features/elections/components/concluded-election-board.tsx
+++ src/features/elections/components/concluded-election-board.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function ConcludedElectionBoard({ children }: Props) {
   return (
     <section className="container mx-auto w-10/12 bg-slate-100 flex flex-col my-4 rounded-md p-4">
-      <header className="grid grid-cols-4 gap-4 border-b-2">
+      <header className="grid grid-cols-4 gap-4">
         <p>Topic</p>
         <p className="text-center">Conclude Vote</p>
       </header>
