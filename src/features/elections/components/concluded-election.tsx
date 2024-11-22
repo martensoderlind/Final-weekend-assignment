961900dd96cb40583c7eff8786baacc3feb5d606
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-22 13:48:22 +0100

    component for concluded elections

---
+++ src/features/elections/components/concluded-election.tsx
@@ -0,0 +1,13 @@
+export default function ConcludedElection() {
+  return (
+    <div className="collapse collapse-arrow join-item border-base-300 border">
+      <input type="radio" name="my-accordion-4" defaultChecked />
+      <div className="collapse-title text-xl font-medium flex justify-between">
+        <h3 className="p-1">Election subject</h3>
+      </div>
+      <div className="collapse-content flex flex-row justify-between">
+        <article>hej</article>
+      </div>
+    </div>
+  );
+}
