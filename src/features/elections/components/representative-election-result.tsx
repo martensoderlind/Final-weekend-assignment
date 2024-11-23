b671dba141566a31ac673dbc8c1e797598a3d103
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-23 18:30:39 +0100

    display the new data

--- src/features/elections/components/representative-election-result.tsx
+++ src/features/elections/components/representative-election-result.tsx
@@ -1,8 +1,16 @@
-export default function RepresentativeElectionResult() {
+import { representativeVote } from "../types";
+
+type Props = {
+  representative: representativeVote;
+};
+
+export default function RepresentativeElectionResult({
+  representative,
+}: Props) {
   return (
     <div className="grid grid-cols-3 gap-4 ">
-      <p className="text-gray-500">Rep name</p>
-      <p className="text-center">15</p>
+      <p className="text-gray-500">{representative.name}</p>
+      <p className="text-center">{representative.voters}</p>
       <p className="text-center">67%</p>
     </div>
   );
