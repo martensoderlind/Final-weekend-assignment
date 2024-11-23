068dae9dd43e6133444f4b47bb2828abaeba678f
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-23 18:30:15 +0100

    name change

--- src/features/elections/components/concluded-election-options.tsx
+++ src/features/elections/components/concluded-election-options.tsx
@@ -1,3 +1,4 @@
+import { getRepresentativeVotes } from "../actions";
 import { ElectionAlternatives } from "../fixtures/mockdb";
 import RepresentativeElectionResult from "./representative-election-result";
 
@@ -5,19 +6,28 @@ type Props = {
   alternative: ElectionAlternatives;
 };
 
-export default function ElectionOptions({ alternative }: Props) {
+export default async function ElectionOptions({ alternative }: Props) {
+  const representatives = await getRepresentativeVotes(
+    alternative.electionId,
+    alternative.choice
+  );
+
   return (
     <div className="mt-2">
       <h3 className="text-l text-gray-800 font-semibold">
         {alternative.choice}
       </h3>
       <header className="grid grid-cols-3 gap-4 border-b-2">
-        <h3 className="text-gray-700">Representativ</h3>
+        <h3 className="text-gray-700">Representativs</h3>
         <h3 className="text-center text-gray-700">Votes</h3>
         <h3 className="text-center text-gray-700">Agreement</h3>
       </header>
-      <RepresentativeElectionResult />
-      <RepresentativeElectionResult />
+      {representatives.map((representative, index) => (
+        <RepresentativeElectionResult
+          key={index}
+          representative={representative}
+        />
+      ))}
     </div>
   );
 }
