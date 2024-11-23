33557f5a5d72ed15dd836680aa5057753975c883
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-23 18:29:53 +0100

    new types

--- src/features/elections/types.ts
+++ src/features/elections/types.ts
@@ -3,3 +3,17 @@ export type ElectionVote = {
   voterId: string;
   choice: string;
 };
+
+export type RepresentativeInformation = {
+  id: string;
+  name: string;
+  voters: number;
+};
+
+export type representativeVote = {
+  id: string;
+  name: string;
+  voters: number;
+  votedinElection: boolean;
+  votedOn: string;
+};
