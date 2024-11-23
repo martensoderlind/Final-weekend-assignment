f2a86861c3e1db3017c5f4531dee615006b89ef1
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-11-23 14:05:38 +0100

    changed structure of ConcludedElectionBoard

--- src/app/(main)/concluded-elections/page.tsx
+++ src/app/(main)/concluded-elections/page.tsx
@@ -1,13 +1,9 @@
-import ConcludedElection from "@/features/elections/components/concluded-election";
 import ConcludedElectionBoard from "@/features/elections/components/concluded-election-board";
 
 export default function page() {
   return (
     <>
-      <ConcludedElectionBoard>
-        <ConcludedElection />
-        <ConcludedElection />
-      </ConcludedElectionBoard>
+      <ConcludedElectionBoard />
     </>
   );
 }
--- src/features/elections/components/concluded-election-board.tsx
+++ src/features/elections/components/concluded-election-board.tsx
@@ -1,15 +1,15 @@
-type Props = {
-  children: React.ReactNode;
-};
+import ConcludedElection from "./concluded-election";
 
-export default function ConcludedElectionBoard({ children }: Props) {
+export default function ConcludedElectionBoard() {
   return (
     <section className="container mx-auto w-10/12 bg-slate-100 flex flex-col my-4 rounded-md p-4">
       <header className="grid grid-cols-4 gap-4">
         <p>Topic</p>
         <p className="text-center">Conclude Vote</p>
       </header>
-      <div className="join join-vertical w-full">{children}</div>
+      <div className="join join-vertical w-full">
+        <ConcludedElection />
+      </div>
     </section>
   );
 }
