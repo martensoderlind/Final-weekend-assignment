33f5268f058ee17d606f203a8aafb7a90d3882cb
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-01 15:24:02 +0100

    removed old functions

--- src/features/elections/actions.ts
+++ src/features/elections/actions.ts
@@ -3,14 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { voteService } from "./instance";
 import { Alternative } from "./types";
-import { user } from "./fixtures/mockdb";
 
-// export async function createRepresentative(formData: FormData) {
-//   const name = formData.get("name") as string;
-//   const email = formData.get("email") as string;
-//   await voteService.createNewRepresentative(name, email);
-//   revalidatePath("/representatives");
-// }
 export async function createElection(formData: FormData) {
   const election = formData.get("election") as string;
   await voteService.createElection(election);
@@ -33,9 +26,4 @@ export async function concludeVote(electionId: string) {
 export async function addElectionOption(electionId: string, newOption: string) {
   await voteService.addElectionOption(electionId, newOption);
   revalidatePath("/elections");
-}
-
-export async function updateVoterRepresentative(representativeId: string) {
-  await voteService.updateVoterRepresentative(user.id, representativeId);
-  revalidatePath("/representatives");
 }
