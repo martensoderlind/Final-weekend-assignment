7c42194ee605e80cce3bcf114163eb64001cbb46
Mårten Söderlind <marten.soderlind@appliedtechnology.se>
2024-12-03 22:00:41 +0100

    removed console.logs

--- src/features/elections/components/representative-election-result.tsx
+++ src/features/elections/components/representative-election-result.tsx
@@ -10,10 +10,11 @@ export default async function RepresentativeElectionResult({
   vote,
   electionId,
 }: Props) {
+  console.log("vote", vote);
   const representatives = await voteService.getRepresentative(vote.voterId!);
   const voters = await voteService.representativeVotes(vote.voterId!);
   const voterAgreement = await voteService.getRatioOfVotersThatAgree(
-    vote.voterId!,
+    vote.representativeId!,
     electionId,
     vote.id!
   );
